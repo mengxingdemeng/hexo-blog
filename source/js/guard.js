b6e953d7746e0f03c7067d71703eed9afe950826
mengxingdemeng <1519259630@qq.com>
2026-04-22 23:26:30 +0800

    修复密码验证界面的问题

--- source/js/guard.js
+++ source/js/guard.js
@@ -1,7 +1,11 @@
 (function() {
-  if (location.pathname.includes('/lock.html')) return;
-  
-  if (!localStorage.blogAuth) {
+  // 白名单：允许访问登录页
+  if (location.pathname.indexOf("lock.html") !== -1) {
+    return;
+  }
+
+  // 如果没登录，强制跳转到登录页
+  if (!localStorage.getItem("blogAuth")) {
     location.replace("/hexo-blog/lock.html");
   }
 })();
