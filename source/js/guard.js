34d83e14587665e31e00d46616581ba05dfff92e
mengxingdemeng <1519259630@qq.com>
2026-04-22 23:56:48 +0800

    修改报错

--- source/js/guard.js
+++ source/js/guard.js
@@ -1,10 +1,10 @@
 (function() {
   // 白名单：允许访问登录页
-  if (location.pathname.indexOf("lock.html") !== -1) {
+  if (location.pathname === "/hexo-blog/lock.html") {
     return;
   }
 
-  // 如果没登录，强制跳转到登录页
+  // 检查登录状态
   if (!localStorage.getItem("blogAuth")) {
     location.replace("/hexo-blog/lock.html");
   }
