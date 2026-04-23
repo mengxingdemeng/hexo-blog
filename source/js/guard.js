bcab68417d0c27624568e9952ac6faa55c3f0a15
mengxingdemeng <1519259630@qq.com>
2026-04-23 10:45:18 +0800

    暂时屏蔽密码登录
    Comment out the guard function to disable login checks.

--- source/js/guard.js
+++ source/js/guard.js
@@ -1,11 +1,11 @@
-(function() {
+//(function() {
   // 白名单：允许访问登录页
-  if (location.pathname === "/hexo-blog/lock.html") {
-    return;
-  }
+//  if (location.pathname === "/hexo-blog/lock.html") {
+//    return;
+//  }
 
   // 检查登录状态
-  if (!localStorage.getItem("blogAuth")) {
-    location.replace("/hexo-blog/lock.html");
-  }
-})();
+//  if (!localStorage.getItem("blogAuth")) {
+ //   location.replace("/hexo-blog/lock.html");
+//  }
+//})();
